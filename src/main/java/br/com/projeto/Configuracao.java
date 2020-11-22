6c153f72cfeb2143f0e0973efd35e78858b340e7
Arlindo Silva <arlindovicente@gmail.com>
2020-11-22 20:56:58 -0300

    Aula 4-11

---
+++ src/main/java/br/com/projeto/Configuracao.java
@@ -0,0 +1,13 @@
+package br.com.projeto;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Configuracao {
+
+    @Bean(name = "applicationName")
+    public String applicationName(){
+        return "Sistema de Vendas";
+    }
+}
